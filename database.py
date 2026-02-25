1241ea099d6d0e6f7fed295b86ec10e3d090146e
Zonetas <zonetasuya@gmail.com>
2026-02-26 00:26:27 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,17 @@
-filters_db = {}
-confess_settings = {}
+import json
+import os
+from config import FILTER_FILE, CONFESS_FILE
+
+def load_json(file):
+    if not os.path.exists(file):
+        return {}
+    with open(file, "r") as f:
+        return json.load(f)
+
+def save_json(file, data):
+    with open(file, "w") as f:
+        json.dump(data, f, indent=4)
+
+filters_db = load_json(FILTER_FILE)
+confess_settings = load_json(CONFESS_FILE)
 confess_cooldown = {}
